9d9aae7dec644df56b4c3757e7c940da5dc33fc6
kpym <kpym.tzanev@gmail.com>
2019-05-07 18:55:43 +0200

    first version

---
+++ deepl.py
@@ -0,0 +1,17 @@
+import sublime
+import sublime_plugin
+import webbrowser
+
+
+class deeplCommand(sublime_plugin.TextCommand):
+    """
+    This will open wwww.deepl.com to translate a word or a selection.
+    """
+    def run(self, edit):
+        region = self.view.sel()[0];
+        if not region.empty():
+            phrase = self.view.substr(region)
+        else:
+            phrase = self.view.substr(self.view.word(region.begin()))
+        sublime.status_message('open DeepL ...')
+        webbrowser.open_new_tab('https://www.deepl.com/translator#en/en/' + phrase)
