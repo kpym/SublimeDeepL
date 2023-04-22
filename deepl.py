c4480ec96cefa07ba99b1b1a4921e675438f9aa9
kpym <kpym.tzanev@gmail.com>
2023-04-22 19:28:59 +0200

    add rewrite command and better string escape

--- deepl.py
+++ deepl.py
@@ -4,7 +4,14 @@ import webbrowser
 import urllib.parse
 
 
-class deeplCommand(sublime_plugin.TextCommand):
+
+
+
+def tourl(str):
+    str = str.replace('/', r'\/').replace('|', r'\|')
+    return urllib.parse.quote(str, safe='')
+
+class DeeplTranslateCommand(sublime_plugin.TextCommand):
     """
     This will open wwww.deepl.com to translate a word or a selection.
     """
@@ -14,5 +21,18 @@ class deeplCommand(sublime_plugin.TextCommand):
             phrase = self.view.substr(region)
         else:
             phrase = self.view.substr(self.view.word(region.begin()))
-        sublime.status_message('open DeepL ...')
-        webbrowser.open_new_tab('https://www.deepl.com/translator#en/en/' + urllib.parse.quote(phrase))
+        sublime.status_message('open DeepL Translate...')
+        webbrowser.open_new_tab('https://www.deepl.com/translator#en/en/' + tourl(phrase))
+
+class DeeplRewriteCommand(sublime_plugin.TextCommand):
+    """
+    This will open wwww.deepl.com to translate a word or a selection.
+    """
+    def run(self, edit):
+        region = self.view.sel()[0];
+        if not region.empty():
+            phrase = self.view.substr(region)
+        else:
+            phrase = self.view.substr(self.view.word(region.begin()))
+        sublime.status_message('open DeepL Rewrite...')
+        webbrowser.open_new_tab('https://www.deepl.com/write/#en/' + tourl(phrase))
